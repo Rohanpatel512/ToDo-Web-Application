50beed8461d7fcae52f19bf7a8721969e323d818
Rohanpatel512 <rohanpat223@gmail.com>
2023-06-30 00:19:39 -0400

    Updated public folder

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Displays either login form or sign up form depending on which was clicked
+ * @param id of form
+ */
+function showForm(formName) {
+
+    console.log(formName + " clicked...");
+
+}
