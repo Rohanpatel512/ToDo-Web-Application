3bbb144b7263afe7e0c086193ddf4bb5445808a4
Rohanpatel512 <rohanpat223@gmail.com>
2023-07-03 22:27:57 -0400

    Finished addTodo function

--- public/js/todo-app.js
+++ public/js/todo-app.js
@@ -6,6 +6,17 @@ const addButton = document.querySelector('#add-btn');
 addButton.addEventListener('click', addTodo);
 
 /**
+ * Enter key is pressed and user has entered todo, add the todo to the list
+ * @param event - The key event being emitted
+ */
+window.addEventListener('keyup', function(event) {
+    const todoValue = document.querySelector('#todo-input').value;
+    if(event.key == 'Enter' && todoValue.length > 0) {
+        addTodo();
+    }
+});
+
+/**
  * Creates a todo container and adds it to the list of todo's
  */
 function addTodo() {
@@ -18,18 +29,25 @@ function addTodo() {
     updateBtn.addEventListener('click', updateTodo);
     deleteBtn.addEventListener('click', deleteTodo);
 
+    // Assign ID's to both buttons
+    updateBtn.id = 'update-btn';
+    deleteBtn.id = 'delete-btn';
+
     // Get the todo user entered
-    const todo = document.getElementById('todo-input').value;
+    const todo = document.querySelector('#todo-input').value;
 
     // Create a new container to store the todo
     listElement.innerHTML = todo;
-    updateBtn.innerText = 'Update';
-    deleteBtn.innerText = 'Delete';
+    updateBtn.innerHTML = '<i class="fa fa-refresh"></i>';
+    deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
 
     // Append all child elements to respectful containers
     listElement.appendChild(updateBtn);
     listElement.appendChild(deleteBtn);
-    listContainer.append(listElement);
+    listContainer.appendChild(listElement);
+
+    // Empty the input field 
+    document.querySelector('#todo-input').value = '';
 
 }
 
