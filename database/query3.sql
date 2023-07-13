72695f5e8ecbf629735523ca8ef6d4d32d63816c
Rohanpatel512 <rohanpat223@gmail.com>
2023-07-13 14:10:18 -0400

    Query to insert new users information into database

---
+++ database/query3.sql
@@ -0,0 +1,5 @@
+/**
+* Inserts new users information into database
+*/
+INSERT INTO todo_app_users.users (firstname, lastname, password, username) 
+VALUES ("{{firstname}}", "{{lastname}}", "{{password}}", "{{username}}"); 
