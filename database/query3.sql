20428c70acef0310548660daf4ca1ff33896c0fc
Rohanpatel512 <rohanpat223@gmail.com>
2023-08-28 20:50:29 -0400

    Deleted query3.sql file.

--- database/query3.sql
+++
@@ -1,5 +0,0 @@
-/**
-* Inserts new users information into database
-*/
-INSERT INTO todo_app_users.users (firstname, lastname, password, username) 
-VALUES ("{{firstname}}", "{{lastname}}", "{{password}}", "{{username}}"); 
